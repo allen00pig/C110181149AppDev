f4e77808e44201d8cb4e85963e57d1d265949f99
allen00pig <allen0pig@gmail.com>
2023-11-29 11:15:43 +0800

    改布局摟

--- app/src/main/java/com/example/c110181149appdev/MainActivity.java
+++ app/src/main/java/com/example/c110181149appdev/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.c110181149appdev;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText FanScore ,FuScore;
+
+    Button Count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FanScore = findViewById(R.id.fanCount);
+        FuScore = findViewById(R.id.fuCount);
+        Count = findViewById(R.id.CountBtn);
     }
 }

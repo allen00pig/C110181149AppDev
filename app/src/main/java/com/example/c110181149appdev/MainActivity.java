f92238dc1becc643e0ec1525007133d863faf778
C110181149 <93571948+allen00pig@users.noreply.github.com>
2023-12-02 02:02:09 +0800

    先給個架構

--- app/src/main/java/com/example/c110181149appdev/MainActivity.java
+++ app/src/main/java/com/example/c110181149appdev/MainActivity.java
@@ -6,13 +6,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText FanScore ,FuScore;
-
-    Button Count;
-
+    private EditText FanScore ,FuScore;
+    private Button Count;
+    private Spinner HomeSpinner ,PeopleSpinner;
+    private Integer Score;
+    private String ManGan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,9 +23,32 @@ public class MainActivity extends AppCompatActivity {
         FanScore = findViewById(R.id.fanCount);
         FuScore = findViewById(R.id.fuCount);
         Count = findViewById(R.id.CountBtn);
+        HomeSpinner = findViewById(R.id.HomeSpinner);
+        PeopleSpinner = findViewById(R.id.PeopleSpinner);
     }
 
     public void BtnClick(View view){
+        String[] Home = getResources().getStringArray(R.array.homeType);
+        String[] People = getResources().getStringArray(R.array.PeopleType);
 
+        int idHome = HomeSpinner.getSelectedItemPosition();
+        int idPeople = PeopleSpinner.getSelectedItemPosition();
+
+        if(idHome == 0 && idPeople == 0){
+            //這是三麻親家
+
+        }
+        if(idHome == 1 && idPeople == 0){
+            //這是三麻閒家
+
+        }
+        if(idHome == 0 && idPeople == 1){
+            //這是四麻親家
+
+        }
+        if(idHome == 1 && idPeople == 1){
+            //這是四麻閒家
+
+        }
     }
 }
